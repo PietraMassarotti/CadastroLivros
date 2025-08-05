80d17b209ae3a8a82b46b54338d4f1d532e75f49
PietraMassarotti <pietramassarotti@gmail.com>
2025-08-05 09:00:36 -0300

    Add files via upload

---
+++ CadastroLivros/utils/IsbnValidacao.php
@@ -0,0 +1,15 @@
+<?php
+class IsbnValidacao
+{
+    public static function validar($numero)
+    {
+        if (!preg_match('/^[0-9]{13,15}$/', $numero)) {
+            echo "O número deve ter entre 13 e 15 dígitos e não pode conter caracteres inválidos.";
+        }
+        if ($numero < 0) {
+            echo "O número não pode ser negativo.";
+        }  
+        return null;
+    }
+}
+?>
