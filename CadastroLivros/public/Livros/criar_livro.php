dd8ea26142e1271ba1950ec1038834f8c7bd235d
PietraMassarotti <pietramassarotti@gmail.com>
2025-08-05 09:01:11 -0300

    Update criar_livro.php

--- CadastroLivros/public/Livros/criar_livro.php
+++ CadastroLivros/public/Livros/criar_livro.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['usuario'])) {
 
 require_once __DIR__ . '/../../models/Livro/LivroDAO.php';
 require_once __DIR__ . '/../../utils/Sanitizacao.php';
+require_once __DIR__ . '/../../utils/IsbnValidacao.php';
 
 $usuario = $_SESSION['usuario']['id'];
 $livroDao = new LivroDAO();
@@ -21,7 +22,7 @@ if (!isset($_SESSION['usuario'])) {
 // Sanitiza as entradas
 $titulo = Sanitizacao::sanitizar($_POST['titulo']);
 $autor = Sanitizacao::sanitizar($_POST['autor']);
-$isbn = Sanitizacao::sanitizar($_POST['isbn']);
+$isbn = IsbnValidacao::validar($_POST['isbn']);
 $ano = Sanitizacao::sanitizar($_POST['ano']);
 
 $LivroDAO = new LivroDAO();
