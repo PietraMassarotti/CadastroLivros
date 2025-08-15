cf35c6b21ad8ba5bb36f377a89e7243476a45066
PietraMassarotti <pietramassarotti@gmail.com>
2025-08-14 22:24:11 -0300

    Update login.php

--- CadastroLivros/public/Login/login.php
+++ CadastroLivros/public/Login/login.php
@@ -8,7 +8,7 @@
 
 <body>
     <h1>Login</h1>
-    <form action="process_login.php" method="POST">
+    <form action="../../controllers/Login/process_login.php" method="POST">
         <label>Email:</label>
         <input type="email" name="email" required>
         <label>Senha:</label>
